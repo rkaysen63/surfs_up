02c76d12d7301ced41bfff3c4562a6d6d0b5826b
rkaysen63 <rkaysen@plant-process.com>
2021-04-30 18:51:50 -0500

    update code

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+
+def hello_world():
+    return 'Hello world'
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+
+def dogs_name():
+    return 'Stella'
+
+if __name__ == '__main__':
+    app.run()
